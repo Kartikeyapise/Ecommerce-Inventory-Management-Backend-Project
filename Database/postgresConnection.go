6ecf075f88a3b6c21193115ecff41a62684fb97e
kartikeya <kartikeya.r@dunzo.com>
2022-04-09 10:23:15 +0530

    database connection

---
+++ Database/postgresConnection.go
@@ -0,0 +1,20 @@
+package Database
+
+import (
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+var err error
+
+func ConnectDatabase() {
+	dsn := "host=localhost user=postgres password=kartikeya dbname=mytestdb port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("Cannot connect to DB")
+	}
+	//DB.AutoMigrate(&User{})
+}
