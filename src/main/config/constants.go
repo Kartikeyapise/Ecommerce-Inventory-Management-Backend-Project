cec81b9f01b40cbd079ad8af6b6c80ad95b9abc1
Kartikeya <kartikeya.software@gmail.com>
2022-05-19 18:02:00 +0530

    Refactoring

--- src/main/config/constants.go
+++ src/main/config/constants.go
@@ -1,6 +1,11 @@
 package config
 
 var (
-	PORT         = ":9000"
-	DATABASE_URL = "host=localhost user=postgres password=kartikeya dbname=postgres port=5432 sslmode=disable TimeZone=Asia/Shanghai"
+	PORT              = ":9000"
+	DATABASE_USER     = "postgres"
+	DATABASE_NAME     = "postgres"
+	DATABASE_PASSWORD = "kartikeya"
+	DATABASE_PORT     = "5432"
+	HOST              = "localhost"
+	DATABASE_URL      = "host=" + HOST + " user=" + DATABASE_USER + " password=" + DATABASE_PASSWORD + " dbname=" + DATABASE_NAME + " port=" + DATABASE_PORT + " sslmode=disable"
 )
